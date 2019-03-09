0aae52318e810eedac926401d96dcd586e3c955a
olivierk95 <keutgensolivier@gmail.com>
2019-03-09 15:56:21 +0100

    fix mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -11,22 +11,37 @@ export default function Header() {
   }
 
   let show = {
-    display: toggle ? "block" : "none"
+    display: toggle ? "flex" : "none"
+  }
+
+  let navigationMenu;
+
+  if (toggle) {
+    navigationMenu = {
+      backgroundColor: "white",
+    };
   }
 
   return (
     <header>
-
-        <button onClick={toggleMenu}>
+      <svg width="100vw" viewBox="0 0 100 20">
+        <polygon points="0,0 100,0 100,20 0,10" className="menu-background"/>
+      </svg>
+      
+      <div className="navigation-menu" style={navigationMenu}>
+        <button onClick={toggleMenu} className="menu-button">
+          <h3 className="menu-title">Menu</h3>
           <BurgerIcon toggle={toggle} />
         </button>
 
-        <div style={ show }>
+        <div style={ show } className="menu">
           <Link to="/" className="menu-item">Accueil</Link>
           <Link to="/projets/" className="menu-item">Projets</Link>
           <Link to="/atelier/" className="menu-item">Atelier</Link>
           <Link to="/contact/" className="menu-item">Contact</Link>
         </div>
+      </div>
+
     </header>
   );
 }
--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -5,12 +5,17 @@ import Header from "../components/header";
 import "../styles/main.scss"
 
 export default ({children}) => 
-    <div>
-        <Header />
-        <Link to="/">
-            <h1>Atelier d'architecture</h1>
-            <h2>Keutgens</h2>
-        </Link>
-        {children}
-    </div>
+    <>
+        <head>
+            <link href="https://fonts.googleapis.com/css?family=Amiri:400,400i,700,700i|Hind:300,400,500,600,700" rel="stylesheet" />
+        </head>
+        <body>
+            <Header />
+            <Link to="/">
+                <h1>Atelier d'architecture</h1>
+                <h2>Keutgens</h2>
+            </Link>
+            {children}
+        </body>
+    </>
     
